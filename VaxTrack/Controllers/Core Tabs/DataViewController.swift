04f6b00a1edde8a40dfcf5ead4b2f646bd66637a
Jo Eun Kim <jkim72@albany.edu>
2021-05-06 03:22:52 -0400

    05-05-2021 Jo Eun Kim [DataVC] Getting/Updating current location, displaying city name.

--- VaxTrack/Controllers/Core Tabs/DataViewController.swift
+++ VaxTrack/Controllers/Core Tabs/DataViewController.swift
@@ -6,24 +6,74 @@
 //
 
 import UIKit
+import CoreLocation
+//import Charts
 
-class DataViewController: UIViewController {
+class DataViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var rankLabel: UILabel!
+    @IBOutlet weak var pieChartView: UIView!
+    
+    let locationManager = CLLocationManager()
+    var coordinates: CLLocationCoordinate2D = CLLocationCoordinate2D()
+    var cityName: String = ""
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        getCurrentLocation()
+        cityNameLabel.text = cityName
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Get address. Now only using locality
+    func getAddress(fromLocation location: CLLocation) {
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location) { placemarks, error in
+            if let error = error {
+                print("error")
+            } else if let placemarks = placemarks {
+                for placemark in placemarks {
+                    let address = placemark.locality
+                    self.cityName = address!
+                    self.cityNameLabel.text = self.cityName
+                }
+            }
+        }
     }
-    */
+    
+    func getCurrentLocation() {
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    // Update current location
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        self.coordinates = CLLocationCoordinate2D.init(latitude: locValue.latitude, longitude: locValue.longitude)
+        
+        if let location = locations.last {
+            getAddress(fromLocation: location)
+        }
+        
+    }
+    
+    
+    
+    
+    // TODO:- For later use in other VC
+//    func getCoordinates(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) -> CLLocationCoordinate2D {
+//            guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return CLLocationCoordinate2D() }
+//            print("locations = \(locValue.latitude) \(locValue.longitude)")
+//        return CLLocationCoordinate2D.init(latitude: locValue.latitude, longitude: locValue.longitude)
+//        }
+
 
 }
