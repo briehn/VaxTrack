cfc055692bf54c9052faef422cf99343be40f015
Brian Yu <briehnyu@gmail.com>
2021-05-09 22:41:52 -0400

    Added Tab Bar Icons (as well as made the Tab Bar look better) New Profile Page that hides certain information if they are a Patient account.

--- VaxTrack/Controllers/Core Tabs/SettingsViewController.swift
+++ VaxTrack/Controllers/Core Tabs/SettingsViewController.swift
@@ -9,13 +9,55 @@ import UIKit
 //import Firebase
 
 class SettingsViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var name: UITextView!
+    @IBOutlet weak var dob: UITextView!
+    @IBOutlet weak var number: UITextView!
+    @IBOutlet weak var email: UITextView!
+    @IBOutlet weak var address: UITextView!
+    @IBOutlet weak var organization: UITextView!
+    @IBOutlet weak var website: UITextView!
+    @IBOutlet weak var operationTime: UITextView!
+    @IBOutlet weak var services: UITextView!
+    @IBOutlet weak var signOut: UIButton!
+    @IBOutlet weak var numberLabel: UITextView!
+    @IBOutlet weak var emailLabel: UITextView!
+    @IBOutlet weak var addresslabel: UITextView!
+    @IBOutlet weak var organizationLabel: UITextView!
+    @IBOutlet weak var websiteLabel: UITextView!
+    @IBOutlet weak var timeLabel: UITextView!
+    @IBOutlet weak var serviceLabel: UITextView!
+    
+    var userType: ST_User.UserType = .PATIENT
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        signOut.isHidden = true //until figure out how to sign out and send to login storyboard
+        
+        if userType == .PATIENT {
+            numberLabel.isHidden = true
+            emailLabel.isHidden = true
+            addresslabel.isHidden = true
+            organizationLabel.isHidden = true
+            websiteLabel.isHidden = true
+            timeLabel.isHidden = true
+            serviceLabel.isHidden = true
+            number.isHidden = true
+            email.isHidden = true
+            address.isHidden = true
+            organization.isHidden = true
+            website.isHidden = true
+            operationTime.isHidden = true
+            services.isHidden = true
+        }
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func signOut(_ sender: Any) {
+        performSegue(withIdentifier: "signOut", sender: sender)
+    }
     
     @IBAction func signOutButtonTapped(_ sender: Any) {
         //let auth = Auth.auth()
